cea4bae4b8887967efe2b998a2060a8f752520bf
Liliana Joseline Del Angel Gomez <lilayos000@gmail.com>
2023-02-02 23:00:22 -0600

    controles

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -7,22 +7,35 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./formulario.component.css'],
 })
 export class FormularioComponent {
-  formularioLogin: FormGroup;
+  formulario: FormGroup;
 
   constructor() {
+    let texto: string = '^[a-z]+@[a-z]+';
     let controles: any = {
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.pattern(texto),
+      ]),
+      apellidos: new FormControl('', [
+        Validators.required,
+        Validators.pattern(texto),
+      ]),
       correo: new FormControl('', [Validators.required, Validators.email]),
       contraseña: new FormControl('', [
         Validators.required,
         Validators.minLength(8),
       ]),
-      // terminos: new FormControl(true, []),
-      //terminos: new FormControl(true),
-      terminos: new FormControl(),
+      confirmar: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
     };
-    this.formularioLogin = new FormGroup(controles);
+    this.formulario = new FormGroup(controles);
   }
-  login() {
-    console.log(this.formularioLogin);
+  enviar() {
+    console.log(this.formulario);
+    if (this.formulario.controls['correo'].errors?.['pattern']) {
+      console.log('llene los datos correctamente');
+    }
   }
 }
