32124d591c913c29695edbaa51521bb445db0e9d
Liliana Joseline Del Angel Gomez <lilayos000@gmail.com>
2023-02-03 00:12:32 -0600

    mensajes

--- src/app/components/formulario/formulario.component.ts
+++ src/app/components/formulario/formulario.component.ts
@@ -8,19 +8,23 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class FormularioComponent {
   formulario: FormGroup;
+  Mensaje: string = '';
 
   constructor() {
-    let texto: string = '^[a-z]+@[a-z]+';
     let controles: any = {
       nombre: new FormControl('', [
         Validators.required,
-        Validators.pattern(texto),
+        Validators.pattern('^[a-zA-Z]+$'),
       ]),
       apellidos: new FormControl('', [
         Validators.required,
-        Validators.pattern(texto),
+        Validators.pattern('^[a-zA-Z]+$'),
       ]),
-      correo: new FormControl('', [Validators.required, Validators.email]),
+      correo: new FormControl('', [
+        Validators.required,
+        Validators.email,
+        Validators.pattern('^[^@]+@[^@]+.[a-zA-Z]{2,}$'),
+      ]),
       contraseña: new FormControl('', [
         Validators.required,
         Validators.minLength(8),
@@ -34,8 +38,18 @@ export class FormularioComponent {
   }
   enviar() {
     console.log(this.formulario);
-    if (this.formulario.controls['correo'].errors?.['pattern']) {
-      console.log('llene los datos correctamente');
+    //if (this.formulario.controls['correo'].errors?.['pattern']) {
+    //  console.log('llene los datos correctamente');
+    //}
+    if (
+      this.formulario.controls['contraseña'].value !=
+      this.formulario.controls['confirmar'].value
+    ) {
+      //console.log('las contraseñas no coinciden');
+      // alert('las contraseñas no coinciden');
+      this.Mensaje = 'las contraseñas no coinciden';
+    } else {
+      this.Mensaje = '';
     }
   }
 }
